09d35a9183defad9c2a8f410b3961123c894348c
Kholmanov Andrei Work <ankholmanov@yandex.ru>
2021-06-09 15:07:05 +0700

    edit contact data

--- components/contact_tpl/top.php
+++ components/contact_tpl/top.php
@@ -1,9 +1,10 @@
-<!--<div class="header__item"><i class="fas fa-map-marker-alt"></i> --><?//= $contact->adress ?><!--</div>-->
+<div class="header__item">
 <?php
 $phones = explode('||', $contact->phone);
 foreach ($phones as $phone) :
     if ($phone !== '') : $phone = explode(':', $phone); ?>
-        <a href="tel:<?= $phone[0] ?>" class="header__item"><i class="fas fa-mobile-alt"></i> <?= $phone[0] ?></a>
+        <a href="tel:<?= $phone[0] ?>" class="mr-2"><i class="fas fa-mobile-alt"></i> <?= $phone[0] ?></a>
     <?php endif ?>
 <?php endforeach ?>
+</div>
 <div class="header__item"><i class="far fa-clock"></i> <?= $contact->work ?></div>
